2987d0e35bbb190355a5b01bf00b5a108bf71de1
Sarthak <sarthak98tyagi@gmail.com>
2019-07-29 17:38:44 +0530

    wh

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -4,23 +4,24 @@ map.addEventListener("touchstart",function(event)
     event.preventDefault();
     if(event.targetTouches.length===2)
     {
-        var x1=event.targetTouches[0].pageX;
-        var y1=event.targetTouches[0].pageY;
-        var x2=event.targetTouches[1].pageX;
-        var y2=event.targetTouches[1].pageY;
-        var initial_distance=Math.sqrt(Math.abs(x2**2-x1**2)+Math.abs(y2**2-y1**2));
+        var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
+        var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
+        var initial_distance=distance(event);
         var final_distance=0;
         map.addEventListener("touchmove",function(event){
            event.preventDefault();
-            var x1=event.targetTouches[0].pageX;
-            var y1=event.targetTouches[0].pageY;
-            var x2=event.targetTouches[1].pageX;
-            var y2=event.targetTouches[1].pageY;
-            final_distance=Math.sqrt(Math.abs(x2**2-x1**2)+Math.abs(y2**2-y1**2));
+           final_distance=distance(event);
             var z=Math.abs(final_distance-initial_distance);
-            map.style.transform="scale("+z+")";
-
         });
-
+        map.style.width=(w+z)+"px";
+        map.style.height=(h+z)+"px";
     }
 });
+function distance(event)
+{
+    var x1=event.targetTouches[0].pageX;
+    var y1=event.targetTouches[0].pageY;
+    var x2=event.targetTouches[1].pageX;
+    var y2=event.targetTouches[1].pageY;
+    return (Math.sqrt(Math.abs(x2**2-x1**2)+Math.abs(y2**2-y1**2)));
+}
