d552adea5d2baa41e380dc7d7f817568536c4180
Sarthak <sarthak98tyagi@gmail.com>
2019-07-29 14:53:22 +0530

    change

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -2,7 +2,6 @@ var map=$(".map-holder");
 map.on("touchstart",function(event)
 {
     event.preventDefault();
-    alert("Hello");
     if(event.targetTouches.length===2)
     {
         alert("Hello");
