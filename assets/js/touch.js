757376b6719e8e4c4eeabc1271763c3827ee4131
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 00:12:55 +0530

    fc7

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -16,7 +16,7 @@ map.addEventListener("touchstart",function(event)
             var y1=event.targetTouches[0].pageY+50;
             var x2=event.targetTouches[1].pageX;
             var y2=event.targetTouches[1].pageY;
-            map.style.transform="zoom("+z+")";
+            map.style.zoom=z;
             var p=x1<x2?x1:x2;
             var q=y1<y2?y1:y2;
             map.style.left='-'+p+'px';
