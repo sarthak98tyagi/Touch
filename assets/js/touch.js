c840a353b05fb35f5d31e1130796c1a7447a08f8
Sarthak <sarthak98tyagi@gmail.com>
2019-07-29 15:30:11 +0530

    update

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -2,13 +2,8 @@ var map=document.getElementById("map-holder");
 map.addEventListener("touchstart",function(event)
 {
     event.preventDefault();
-<<<<<<< HEAD
     if(event.targetTouches.length===2)
     {
         console.log("Double Touch");
     }
 });
-=======
-    alert("Hello");
-});
->>>>>>> 7e5f200d4ca224857ebdc71d7e12b1ea69ecffca
