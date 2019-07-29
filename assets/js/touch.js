cf8fc5ed53c165bf8d20bba6e39011fd60403d27
Sarthak <sarthak98tyagi@gmail.com>
2019-07-29 16:36:47 +0530

    abs

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -2,13 +2,13 @@ var map=document.getElementById("map-holder");
 map.addEventListener("touchstart",function(event)
 {
     event.preventDefault();
-    if(event.targetTouches.length===2)
+    if(event.targetTouches.length===1)
     {
         var x1=event.targetTouches[0].pageX;
         var y1=event.targetTouches[0].pageY;
         var x2=event.targetTouches[1].pageX;
         var y2=event.targetTouches[1].pageY;
-        var initial_distance=Math.sqrt((x2**2-x1**2)+(y2**2-y1**2));
+        var initial_distance=Math.sqrt(Math.abs(x2**2-x1**2)+Math.abs(y2**2-y1**2));
         var final_distance=0;
         map.addEventListener("touchmove",function(event){
            event.preventDefault();
@@ -16,8 +16,8 @@ map.addEventListener("touchstart",function(event)
             var y1=event.targetTouches[0].pageY;
             var x2=event.targetTouches[1].pageX;
             var y2=event.targetTouches[1].pageY;
-            final_distance=Math.sqrt((x2**2-x1**2)+(y2**2-y1**2));
-            alert(final_distance-initial_distance);
+            final_distance=Math.sqrt(Math.abs(x2**2-x1**2)+Math.abs(y2**2-y1**2));
+            alert(Math.abs(final_distance-initial_distance));
 
         });
 
