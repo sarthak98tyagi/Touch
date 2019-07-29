f47846013874702297fca4000cf331477fb53a40
Sarthak <sarthak98tyagi@gmail.com>
2019-07-29 16:12:34 +0530

    new update

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -4,6 +4,22 @@ map.addEventListener("touchstart",function(event)
     event.preventDefault();
     if(event.targetTouches.length===2)
     {
-        alert("Double Touch");
+        var x1=event.targetTouches[0].pageX;
+        var y1=event.targetTouches[0].pageY;
+        var x2=event.targetTouches[1].pageX;
+        var y2=event.targetTouches[1].pageY;
+        var initial_distance=Math.sqrt((x2**2-x1**2)+(y2**2-y1**2));
+        var final_distance=0;
+        map.addEventListener("touchmove",function(event){
+           event.preventDefault();
+            var x1=event.targetTouches[0].pageX;
+            var y1=event.targetTouches[0].pageY;
+            var x2=event.targetTouches[1].pageX;
+            var y2=event.targetTouches[1].pageY;
+            final_distance=Math.sqrt((x2**2-x1**2)+(y2**2-y1**2));
+            alert(final_distance-initial_distance);
+
+        });
+
     }
 });
