a7f2b2448073dcc946bb13890966b79525cb74e6
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 16:39:01 +0530

    ham3

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,9 +1,21 @@
 var map=document.getElementById("map");
 var ham= new Hammer(map);
 ham.get('pinch').set({enable:true});
-ham.on('pinchin',function(){
-   alert("In");
+var width=getComputedStyle(map).getPropertyValue("width");
+var height=getComputedStyle(map).getPropertyValue("height");
+var lmar=50;
+var tmar=50;
+ham.on('pinch',function(e)
+{
+    map.style.width=(width*e.scale)+'px';
+    map.style.height=(height*e.scale)+'px';
+    map.style.marginLeft=(-lmar*e.scale)+'px';
+    map.style.marginTop=(-tmar*e.scale)+'px';
 });
-ham.on('pinchout',function(){
-    alert("Out");
+ham.on('pinchend',function(e)
+{
+    map.style.width=(width*e.scale)+'px';
+    map.style.height=(height*e.scale)+'px';
+    map.style.marginLeft=(lmar*e.scale)+'px';
+    map.style.marginTop=(tmar*e.scale)+'px';
 });
