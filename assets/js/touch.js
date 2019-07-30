bec911bfb95ddcd5eb71fb7b9ca338cabaccb2ad
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 18:05:30 +0530

    ham7

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -3,11 +3,10 @@ var holder=document.getElementById("map-holder");
 var dwidth=getComputedStyle(holder).getPropertyValue("width");
 var ham= new Hammer(map);
 ham.get('pinch').set({enable:true});
-var width=getComputedStyle(map).getPropertyValue("width");
-var height=getComputedStyle(map).getPropertyValue("height");
-ham.on('pinch',function(e)
+var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
+var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
+ham.on('tap',function(e)
 {
         map.style.width=(width*e.scale)+'px';
         map.style.height=(height*e.scale)+'px';
-
 });
