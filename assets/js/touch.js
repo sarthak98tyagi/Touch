f49d17a0e6cf8163adcbbd08aba1572dd09f07da
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 12:11:44 +0530

    new

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -3,19 +3,16 @@ var z=1;
 map.addEventListener("touchstart",function(event)
 {
     event.preventDefault();
-    if(event.targetTouches.length===1)
+    if(event.targetTouches.length===2)
     {
 
-        z=z+0.5;
         map.addEventListener("touchmove",function(event){
             event.preventDefault();
             var x1=event.targetTouches[0].pageX+50;
             var y1=event.targetTouches[0].pageY+50;
             var x2=event.targetTouches[1].pageX;
             var y2=event.targetTouches[1].pageY;
-            map.style.transform="scaleX("+z+")";
-            var p=x1<x2?x1:x2;
-            var q=y1<y2?y1:y2;
+            map.style.width=(x1+x2)+"px";
 
          });
 
