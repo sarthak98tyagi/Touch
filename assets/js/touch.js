3022f873f7a4b15100865e79b0c4a3cae9b662ba
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 17:07:56 +0530

    ham5

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,24 +1,20 @@
 var map=document.getElementById("map");
+var holder=document.getElementById("map-holder");
+var dwidth=getComputedStyle(holder).getPropertyValue("width");
 var ham= new Hammer(map);
 ham.get('pinch').set({enable:true});
 var width=getComputedStyle(map).getPropertyValue("width");
 var height=getComputedStyle(map).getPropertyValue("height");
 var lmar=50;
 var tmar=50;
-ham.on('tap',function(e){
-    console.log(e.scale);
-});
-ham.on('pinch',function(e)
+ham.on('tap',function(e)
 {
-    map.style.width=(width*e.scale)+'px';
-    map.style.height=(height*e.scale)+'px';
-    map.style.marginLeft=(-lmar*e.scale)+'px';
-    map.style.marginTop=(-tmar*e.scale)+'px';
-});
-ham.on('pinchend',function(e)
-{
-    map.style.width=(width*e.scale)+'px';
-    map.style.height=(height*e.scale)+'px';
-    map.style.marginLeft=(lmar*e.scale)+'px';
-    map.style.marginTop=(tmar*e.scale)+'px';
+    if(width*e.scale >= dwidth)
+    {
+        map.style.width=(width*e.scale)+'px';
+        map.style.height=(height*e.scale)+'px';
+        map.style.marginLeft=(-lmar*e.scale)+'px';
+        map.style.marginTop=(-tmar*e.scale)+'px';
+    }
+
 });
