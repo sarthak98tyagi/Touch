f1c0c2d6769110e0e517428666e2a9a985f433f7
Sarthak <sarthak98tyagi@gmail.com>
2019-07-30 16:03:48 +0530

    ham2

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,6 +1,9 @@
 var map=document.getElementById("map");
 var ham= new Hammer(map);
 ham.get('pinch').set({enable:true});
-ham.on('pinch',function(){
-   alert("working");
+ham.on('pinchin',function(){
+   alert("In");
+});
+ham.on('pinchout',function(){
+    alert("Out");
 });
