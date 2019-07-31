bde7e7ccb153bdc59b1e484883d5ed53bde3c8c9
Sarthak <sarthak98tyagi@gmail.com>
2019-07-31 17:05:52 +0530

    pos30

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -4,26 +4,26 @@ var dwidth=parseInt(getComputedStyle(holder).getPropertyValue("width"));
 var dheight=parseInt(getComputedStyle(holder).getPropertyValue("height"));
 var ham= new Hammer(map);
 ham.get('pinch').set({enable:true});
-var init_width=parseInt(getComputedStyle(map).getPropertyValue("width"));
-var init_height=parseInt(getComputedStyle(map).getPropertyValue("height"));
+
 var mleft=50;
 var mtop=50;
 ham.on('pinch',function(e)
 {
-    
+
     var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
     var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
         if(width*e.scale >= dwidth && height*e.scale >= dheight)
         {
+
             map.style.width=(width*e.scale)+'px';
             map.style.height=(height*e.scale)+'px';
             var x=e.center['x'];
             var y=e.center['y'];
-            var l=parseInt(getComputedStyle(map).getPropertyValue("margin-left"))-((x*e.scale)/2);
-            var t=parseInt(getComputedStyle(map).getPropertyValue("margin-top"))-((y*e.scale)/2);
+            var l=width*e.scale-width;
+            var t=height*e.scale-height;
             console.log(l,t);
-            map.style.marginLeft=l+'px';
-            map.style.marginTop=t+'px';
+            map.style.marginLeft=(l-e.scale*x)+'px';
+            map.style.marginTop=(t-e.scale*y)+'px';
 
         }
 
