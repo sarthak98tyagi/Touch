9dd7beae1cea3954b0cc84835c5d7e3f4be47f6e
Sarthak <sarthak98tyagi@gmail.com>
2019-07-31 19:43:41 +0530

    base5

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -7,26 +7,25 @@ ham.get('pinch').set({enable:true});
 
 var mleft=50;
 var mtop=50;
-ham.on('pinch',function(e)
+ham.on('pinchstart',function(e)
 {
 
     var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
     var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
         if(width*e.scale >= dwidth && height*e.scale >= dheight)
         {
-
             map.style.width=(width*e.scale)+'px';
             map.style.height=(height*e.scale)+'px';
-            // var x=e.center['x'];
-            // var y=e.center['y'];
-            // var ml=parseInt(getComputedStyle(map).getPropertyValue("margin-left"));
-            // var mt=parseInt(getComputedStyle(map).getPropertyValue("margin-top"));
-            // var l=ml-(x*e.scale)/2;
-            // var t=mt-(y*e.scale)/2;
-            // // l=-(ml+Math.abs(ml-l));
-            // // t=-(mt+Math.abs(mt-t));
-            // map.style.marginLeft=l+'px';
-            // map.style.marginTop=t+'px';
+            var x=e.center['x'];
+            var y=e.center['y'];
+            var ml=parseInt(getComputedStyle(map).getPropertyValue("margin-left"));
+            var mt=parseInt(getComputedStyle(map).getPropertyValue("margin-top"));
+            var l=-(x*e.scale)/2;
+            var t=-(y*e.scale)/2;
+            // l=-(ml+Math.abs(ml-l));
+            // t=-(mt+Math.abs(mt-t));
+            map.style.marginLeft=l+'px';
+            map.style.marginTop=t+'px';
         }
 
 });
