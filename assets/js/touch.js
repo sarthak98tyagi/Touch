fe83baefe0a742fc6ef9cf1d07864fc4b27f1e89
Sarthak <sarthak98tyagi@gmail.com>
2019-07-31 15:02:19 +0530

    pos10

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -25,14 +25,15 @@ ham.on('pinch',function(e)
     var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
         if(width*e.scale >= dwidth && height*e.scale >= dheight)
         {
-            alert(e.center);
+            console.log(e.center['x'],e.center['y'])
             map.style.width=(width*e.scale)+'px';
             map.style.height=(height*e.scale)+'px';
-            // alert(getComputedStyle(map).getPropertyValue("width"));
-            // alert(getComputedStyle(map).getPropertyValue("height"));
-            // alert(e.center);
-            // var x=e.center['x'];
-            // var y=e.center['y'];
+            alert(getComputedStyle(map).getPropertyValue("width"));
+            alert(getComputedStyle(map).getPropertyValue("height"));
+            alert(e.center);
+            var x=e.center['x'];
+            var y=e.center['y'];
+            console.log(x,y);
             // if(parseInt(getComputedStyle(map).getPropertyValue("width"))<=init_width ||
             // parseInt(getComputedStyle(map).getPropertyValue("height"))<=init_height)
             // {
@@ -45,7 +46,7 @@ ham.on('pinch',function(e)
             //
             //     map.style.marginLeft=-x+'px';
             //     map.style.marginTop=-y+'px';
-            // }
+            }
         }
 });
 // ham.on('pinchend',function(e)
