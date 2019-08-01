950fd9420fc91d598c12088ca7939d272a0d6dc5
Sarthak <sarthak98tyagi@gmail.com>
2019-08-01 17:02:37 +0530

    done

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -26,25 +26,38 @@ ham.on('pinchmove',function(e)
             var t=(y*e.scale);
             var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
              var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
-            var lm=(ml+Math.abs(ml-l));
-            var tm=(mt+Math.abs(mt-t));
-            var xgap=w-lm;
-            var ygap=h-tm;
-            if(xgap<dwidth && ygap<dheight)
+            lm=(ml+Math.abs(ml-l));
+            tm=(mt+Math.abs(mt-t));
+            var xgap=(w-dwidth);
+            var ygap=(h-dheight);
+            console.log(l,t,xgap,ygap);
+            if(l<(xgap) && t<(ygap))
             {
                 map.style.marginLeft=-lm+'px';
                 map.style.marginTop=-tm+'px';
             }
         }
 });
-// ham.on('swiperight',function(e)
-// {
+// ham.on('pinchend',function(e){
+//     var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
+//     var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
+//     map.style.width=(width*e.scale)+'px';
+//     map.style.height=(height*e.scale)+'px';
+//     var x=(e.center['x']*e.scale)/2;
+//     var y=(e.center['y']*e.scale)/2;
 //     var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
 //     var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
-//     var ml=parseInt(getComputedStyle(map).getPropertyValue("margin-left"));
-//     var mt=parseInt(getComputedStyle(map).getPropertyValue("margin-top"));
-//     if(ml>0)
+//     // l=-(ml+Math.abs(ml-l));
+//     // t=-(mt+Math.abs(mt-t));
+//     console.log(w,dwidth,h,dheight);
+//     var xgap=(w-dwidth);
+//     var ygap=(h-dheight);
+//     if(l<xgap && t<ygap)
 //     {
-//         map.style.marginLeft=-ml+'px';
+//         map.style.marginLeft=x+'px';
+//         map.style.marginTop=y+'px';
 //     }
+//
 // });
+
+
