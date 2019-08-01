6ea4f3609d248aba84206541840ab8b288562042
Sarthak <sarthak98tyagi@gmail.com>
2019-08-01 17:58:20 +0530

    s15

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -21,14 +21,14 @@ ham.on('pinchmove',function(e)
             var y=e.center['y'];
             var ml=parseInt(getComputedStyle(map).getPropertyValue("margin-left"));
             var mt=parseInt(getComputedStyle(map).getPropertyValue("margin-top"));
-            var l=(x*e.scale);
-            var t=(y*e.scale);
+            var l=(x*e.scale)/2;
+            var t=(y*e.scale)/2;
             var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
              var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
             var lm=(ml+Math.abs(ml-l));
             var tm=(mt+Math.abs(mt-t));
             var xgap=w-lm;
-            var ygap=h-lt;
+            var ygap=h-tm;
             if(xgap<dwidth && ygap<dheight)
             {
                 map.style.marginLeft=-lm+'px';
