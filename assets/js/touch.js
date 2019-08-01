492bf730af5cafe1e4d054bd7117654a639974d9
Sarthak <sarthak98tyagi@gmail.com>
2019-08-01 15:55:00 +0530

    outflow9

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -26,8 +26,8 @@ ham.on('pinchmove',function(e)
             var t=(y*e.scale);
             var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
              var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
-            // l=-(ml+Math.abs(ml-l));
-            // t=-(mt+Math.abs(mt-t));
+            l=-(ml+Math.abs(ml-l));
+            t=-(mt+Math.abs(mt-t));
             console.log(w,dwidth,h,dheight);
             var xgap=(w-dwidth);
             var ygap=(h-dheight);
@@ -39,26 +39,26 @@ ham.on('pinchmove',function(e)
             }
         }
 });
-ham.on('pinchend',function(e){
-    var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
-    var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
-    map.style.width=(width*e.scale)+'px';
-    map.style.height=(height*e.scale)+'px';
-    var x=(e.center['x']*e.scale)/2;
-    var y=(e.center['y']*e.scale)/2;
-    var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
-    var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
-    // l=-(ml+Math.abs(ml-l));
-    // t=-(mt+Math.abs(mt-t));
-    console.log(w,dwidth,h,dheight);
-    var xgap=(w-dwidth);
-    var ygap=(h-dheight);
-    if(l<xgap && t<ygap)
-    {
-        map.style.marginLeft=x+'px';
-        map.style.marginTop=y+'px';
-    }
-
-});
+// ham.on('pinchend',function(e){
+//     var width=parseInt(getComputedStyle(map).getPropertyValue("width"));
+//     var height=parseInt(getComputedStyle(map).getPropertyValue("height"));
+//     map.style.width=(width*e.scale)+'px';
+//     map.style.height=(height*e.scale)+'px';
+//     var x=(e.center['x']*e.scale)/2;
+//     var y=(e.center['y']*e.scale)/2;
+//     var w=parseInt(getComputedStyle(map).getPropertyValue("width"));
+//     var h=parseInt(getComputedStyle(map).getPropertyValue("height"));
+//     // l=-(ml+Math.abs(ml-l));
+//     // t=-(mt+Math.abs(mt-t));
+//     console.log(w,dwidth,h,dheight);
+//     var xgap=(w-dwidth);
+//     var ygap=(h-dheight);
+//     if(l<xgap && t<ygap)
+//     {
+//         map.style.marginLeft=x+'px';
+//         map.style.marginTop=y+'px';
+//     }
+//
+// });
 
 
