dd4a72ea20f11e8c312eef75891653851cc0ddf2
Sarthak <sarthak98tyagi@gmail.com>
2019-08-02 14:51:50 +0530

    left7

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -23,12 +23,12 @@ ham.on('pinchmove',function(e)
     var mt=parseInt(getComputedStyle(map).getPropertyValue("top"));
     var lm=(ml-l);
     var tm=(mt-t);
-        if( (w-lm) > dwidth && (h-tm) > dheight)
+        if( (w+lm) > dwidth && (h+tm) > dheight)
         {
             map.style.width=(width*e.scale)+'px';
             map.style.height=(height*e.scale)+'px';
-            map.style.left=-lm+'px';
-            map.style.top=-tm+'px';
+            map.style.left=lm+'px';
+            map.style.top=tm+'px';
         }
 });
 ham.on('swiperight',function(e)
