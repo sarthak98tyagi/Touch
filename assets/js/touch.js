56a53156b2cb44b2b263653946cae2edbeee606d
Sarthak <sarthak98tyagi@gmail.com>
2019-08-06 18:49:37 +0530

    mark40

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -121,8 +121,8 @@ ham.on('tap',function(e)
     e.preventDefault();
     var x=e.center['x'];
     var y=e.center['y'];
-    var cl=parseFloat(getComputedStyle(map).getPropertyValue("left"));
-    var ct=parseFloat(getComputedStyle(map).getPropertyValue("top"));
+    var cl=parseFloat(map.style.left);
+    var ct=parseFloat(map.style.top);
     console.log(ct);
     var xmark=Math.abs(cl)+x;
     var ymark=Math.abs(ct)+y;
