dad92483bb77296dc10ef015484cc1e50e727897
Sarthak <sarthak98tyagi@gmail.com>
2019-08-08 16:43:23 +0530

    up2

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -48,7 +48,7 @@ ham.on('pinchmove',function(e)
     var y=e.center['y'];
     var l=(x*e.scale);
     var t=(y*e.scale);
-        if( (w-l) >= dwidth && width<6000)
+        if( (w-l) >= dwidth && width<3000)
         {
             map.style.width=w+'px';
             map.style.left=-l+'px';
