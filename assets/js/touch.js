9eeff02095fb132e95feee575dcc2c3662fe7ec4
Sarthak <sarthak98tyagi@gmail.com>
2019-08-13 19:42:18 +0530

    dr114

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -222,9 +222,9 @@ ham.on('tap',function(e)
                     e.target.style.top=dim.clientY+'px';
                 }
             });
-        ham.set({ enable: true});
-        pham.set({enable: true});
-        e.target.style.background="transparent";
+        // ham.set({ enable: true});
+        // pham.set({enable: true});
+        // e.target.style.background="transparent";
     });
         mp=mp+1;
 });
