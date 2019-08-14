d1266b526b95e7b9a5fa28871d367c8f95e813e9
Sarthak <sarthak98tyagi@gmail.com>
2019-08-14 14:24:04 +0530

    drag50

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -234,9 +234,12 @@ ham.on('tap',function(e)
                                 if((dim.clientX-k)>(-30))
                                 {   var mr=parseFloat(map.style.right);
                                     if((mr+Math.abs(dim.clientX-k))<=0)
-                                    var ml=parseFloat(map.style.left);
-                                    map.style.left=(ml-Math.abs(dim.clientX-k))+'px';
-                                    scaleshift();
+                                    {
+                                        var ml=parseFloat(map.style.left);
+                                        map.style.left=(ml-Math.abs(dim.clientX-k))+'px';
+                                        scaleshift();
+                                    }
+
                                 }
                         }
                         var xmark=Math.abs(cl)+dim.clientX;
