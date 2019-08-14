cbdfbfc394a41623d0ef7ef039e031ffcdae4b1d
Sarthak <sarthak98tyagi@gmail.com>
2019-08-14 14:50:14 +0530

    drag59

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -234,7 +234,7 @@ ham.on('tap',function(e)
                                 {
                                     var mr=parseFloat(getComputedStyle(map).getPropertyValue("right"));
                                     console.log(mr);
-                                    if((mr+Math.abs(dim.clientX-k)+20)<=0)
+                                    if((mr+Math.abs(dim.clientX-k))<=0)
                                     {
                                         var ml=parseFloat(map.style.left);
                                         map.style.left=(ml-Math.abs(dim.clientX-k))+'px';
