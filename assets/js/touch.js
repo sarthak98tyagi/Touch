9784686d5494ff49d95cdb577a49412ec0d3140a
Sarthak <sarthak98tyagi@gmail.com>
2019-08-19 16:14:40 +0530

    z32

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -93,19 +93,18 @@ ham.on('pinchmove',function(e)
     var h=(height*e.scale);
     var x=e.center['x'];
     var y=e.center['y'];
-    var xcor=Math.abs(parseFloat(map.style.left))+x;
-    var ycor=Math.abs(parseFloat(map.style.top))+y;
+    var xcor=parseFloat(map.style.left)+x;
+    var ycor=parseFloat(map.style.top)+y;
     var nxcor=(xcor/width)*w;
     var nycor=(ycor/height)*h;
-    console.log(nycor,height);
     var tl=nxcor-x;
     var tt=nycor-y;
-        if( (w-tl) >= dwidth && (h-tt) >= dheight)
+        if( (w+tl) >= dwidth && (h+tt) >= dheight)
         {
 
             map.style.width=w+'px';
-            map.style.left=-tl+'px';
-            map.style.top=-tt+'px';
+            map.style.left=tl+'px';
+            map.style.top=tt+'px';
             scaleshift(e.scale);
         }
 });
