81500426d039bd61d97b420afc0fbad4b382bab5
Sarthak <sarthak98tyagi@gmail.com>
2019-08-20 14:37:45 +0530

    z73

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -99,7 +99,7 @@ ham.on('pinchmove',function(e)
     var ycor=Math.abs(parseFloat(map.style.top))+y;
     var nxcor=((xcor/width)*w);
     var nycor=((ycor/height)*h);
-    console.log(getComputedStyle(map).getPropertyValue('right'));
+    console.log(parseFloat(getComputedStyle(map).getPropertyValue('right')*e.scale));
     var tl=(nxcor-x);
     var tt=(nycor-y);
         if( (w-tl) >= dwidth && (h-tt) >= dheight)
