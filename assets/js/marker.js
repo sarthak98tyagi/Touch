d2e7ee776d0c6e55fffa4b512fef00bf8ad26630
Sarthak <sarthak98tyagi@gmail.com>
2019-08-30 15:24:13 +0530

    cl45

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -174,11 +174,13 @@ class Marker
                 }
             }.bind(this);
             map.addEventListener('click', mapclick);
-            map.addEventListener('touchstart',this.pinchstart);
-            map.addEventListener('touchmove',this.pinchmove);
+            map.addEventListener('touchstart',this.pinchstart.bind(this));
+            map.addEventListener('touchmove',this.pinchmove.bind(this));
     }
     pinchstart(e)
     {
+        this.x=0;
+        this.y=0;
         e.preventDefault();
         if(e.targetTouches.length===2)
         {
